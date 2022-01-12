aaace3f8f0c62ed7909b3b459e57c3ffe127ba81
Diana Aliyeva <76077598+Dianliyeva@users.noreply.github.com>
2022-01-11 20:01:42 -0500

    First Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/flipcoin', function(req, res){
+    res.send("Hello from the root application URL");
+});
+
+app.use(express.static('public'));
+
+app.listen(port, () => {
+    console.log(`Server is listening at //localhost:3000`)
+}) 
